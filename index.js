746f29b15678619e0ec5e7a67e08e7df39df531e
Aziza  iliasova <miacorporation25@gmail.com>
2021-06-16 11:55:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,14 +18,14 @@
      return ( this.radius * 2 ) * pi
    }
     set circumference(circumference){
-     this.radius = circumference / ( pi * 2 ) 
+     this.radius = circumference / ( pi * 2 )
    }
 
    get area (){
      return pi * ( this.radius * this.radius )
    }
     set area (area){
-    this.radius = Math.sqrt( area / pi ) 
+    this.radius = Math.sqrt( area / pi )
    }
 
 
